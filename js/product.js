7e50463d710024493ba77f62499b7db3560e6280
Maja Berendsen <maja498h@stud.kea.dk>
2021-08-31 00:29:27 +0200

    we are getting there

--- js/product.js
+++ js/product.js
@@ -1,12 +1,12 @@
 const url = "http//kea-alt-del.dk/t7/api/products/2801"
 
-fetch (url)
-      .then(res=>res.json())
-      .then(data=>showProduct(data))
+fetch(url)
+    .then(res => res.json())
+    .then(data => showProduct(data))
 
-function showProduct(product){
-    document.querySelector(".display .productname").textContent= product.brandbio;
-    document.querySelector(".display .productbrand").textContent= product.brandname;
-    document.querySelector(".display .product.image").textContent= product.image;
-    document.querySelector(".display .productcategory").textContent=product.category;
+function showProduct(product) {
+    document.querySelector(".display .productname").textContent = product.brandbio;
+    document.querySelector(".display .productbrand").textContent = product.brandname;
+    document.querySelector(".display .product.image").textContent = product.image;
+    document.querySelector(".display .productcategory").textContent = product.category;
 }
